f8f7aa9e2b72d8dcef014634646d15ef4fdc986a
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-20 16:02:02 +0000

    Broke out link rendering from NoteBuddy to Renderer

--- src/noteBuddy.js
+++ src/noteBuddy.js
@@ -7,39 +7,17 @@
     return this.list.push(note);
   };
 
-  NoteBuddy.prototype.createNote = function(string, noteConstructor = Note) {
+  NoteBuddy.prototype.createNote = function(string, noteConstructor = Note, callback) {
     var note = new noteConstructor(string);
     this.storeNote(note);
+    callback && callback()
     return note;
   };
 
   NoteBuddy.prototype.noteList = function() {
     var htmlNoteList = this.renderList()
-    return htmlNoteList.join("");
+    return htmlNoteList
   };
-
-  NoteBuddy.prototype.singleNote = function (noteIndex) {
-    var note = this.list[noteIndex];
-    return "<div class='single_note'>" + note.returnNote() + "</div>";
-  };
-
-  var renderLink = function(note,index){
-    link = "<a href='#$" + index + "'>" + note.returnNote().substring(0, 20) + "</a>"
-    return link
-  }
-
-  NoteBuddy.prototype.renderList = function(){
-    var noteListHtml = ["<ul class='note_list'>"];
-    var notes = this.list;
-    notes.forEach(function(note, index) {
-      noteListHtml.push(
-        renderLink(note,index)
-      );
-    });
-    noteListHtml.push("</ul>");
-
-    return noteListHtml
-  }
 
   exports.NoteBuddy = NoteBuddy;
 })(this);
--- src/renderer.js
+++ src/renderer.js
@@ -8,5 +8,18 @@ Renderer.prototype.view = function(values) {
   }
 };
 
+var renderLink = function(note,index){
+  link = "<a href='#$" + index + "'>" + note.returnNote().substring(0, 20) + "</a>"
+  return link
+}
+
+Renderer.prototype.renderLinks = function(notes){
+  var renderedLinks = ""
+  notes.forEach(function(note, index) {
+      renderedLinks += renderLink(note,index)
+  });
+  return "<ul class='note_list'>" + renderedLinks + "</ul>"
+}
+
 exports.Renderer = Renderer;
 })(this);
